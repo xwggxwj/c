1e5d554f449a5feb5f4850a4da09e6876b42c793
xwggxwj <128921511+xwggxwj@users.noreply.github.com>
2023-03-27 20:12:45 +0800

    Add files via upload

--- test/test/test.c
+++ test/test/test.c
@@ -1,82 +1,42 @@
 #include <stdio.h>
-//int main()
+//int main ()
 //{
-//	int num1=10;
-//	int num2=20;
-//	if (num1>num2)
-//		{
-//			printf("%d\n",num1);
-//		}
-//	else
-//		{
-//			printf("%d\n",num2);
-//		}
-//	return 0;
-//}
-//int Max(unsigned int a,unsigned int b)
-//{
-//	if(a>b)
+//	int age=17;
+//	/*if(age<19)
+//		printf("未成年\n");
+//	else if (age>=19 && age<28)
+//		printf("青年\n");
+//	else if(age>=28 && age<50)
+//		printf("壮年\n");
+//	return 0;*/
+//	if(age<18)
 //	{
-//		printf("%d\n",a);
-//	}
-//	else
-//	{
-//		printf("%d\n",b);
+//		printf("未成年\n");
+//		printf("不能谈恋爱\n");
 //	}
 //}
+////if语句
 //int main()
 //{
-//	Max(10,5);
-//	return 0;
-//}//比较两个字的大小
-// 
-//int main()
-//{
-//	int a=0;//四个字节，00000000000000000000000000000000000
-//	int b=~a;//有符号的整形,内存里存储的是二进制补码，打印的是这个是这个数的源码
-//	printf("%d\n",b);
-//	return 0;
-//}
-//int main()
-//{
-//	int a=10;
-//	int b=a++;//后置++，先试用。再++，前置++，先++，再使用，后置——一样使用
-//	printf("a=%d b=%d\n",a,b);//结果是11，10，前置加加结果是11,11
-//	return 0;
-//}
-//int main()
-//{
-//	int a=(int)3.14;
-//	//return 0;
-//}
-//int main()
-//{
-//	int a=3;
-//	int b=5;
-//	int c=a&&b;
-//	printf("%d\n",c);//并且，a是真并且b是真，输出结果是1，逻辑或||是或者；
-//	return 0;
-//}
-//关键字，
-//void test()
-//{
-//	static int a=1;
-//	a++;
-//	printf("a=%d\n",a);
-//}
-//int main()
-//{
-//	int i=0;
-//	while(i<5)
-//	{
-//		test();
-//		i++;
+//	int a=0;
+//	int b=2;
+//	if(a==1)
+//	{	if(b==2)
+//			printf("hehe\n");
 //	}
-//	return 0;
+//		else
+//		printf("haha\n");
+//return 0;
+//}
+//if(condition)     if(condition)
+//{					{
+//	return x;			return x;
+//}					}
+//else					return y;    //这两个代码是相同的，else可以审阅
+//{
+//	return y;
 //}
 int main()
 {
-	extern int g_val;
-	printf("g_val %d\n",g_val);
-	return 0;
+	
 }
